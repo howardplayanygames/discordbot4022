d072d99e2339792f68c57f689321faff1effa68e
howardplayanygames <97337613+howardplayanygames@users.noreply.github.com>
2022-12-24 22:41:54 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,5 +35,4 @@ for filename in os.listdir('./cmds'):
        bot.load_extension(f'cmds.{filename[:-3]}')
 
 if __name__ == "__main__":
-   token = os.getenv("TOKEN")
-   bot.run(token)
+   bot.run(jdata['TOKEN'])
